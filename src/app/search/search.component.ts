ebdc73e3688a0ff5506afb30fdc192cb5b50e288
Dmitri Tikhonov <dima1mim@gmail.com>
2018-08-20 19:36:06 +0300

    Update search.component.ts

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -7,62 +7,58 @@ import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/startWith';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
+
 export class SearchComponent implements OnInit {
 
-  @Output() filmN;
+    @Output() filmN;
 
     myControl = new FormControl();
     filteredOptions: Observable<any>;
     searchFilms = [];
     searchList = [];
 
-  constructor(private configService:ConfigService) { }
+  constructor(private configService:ConfigService) {
+      this.myControl.valueChanges
+      .distinctUntilChanged()
+      .debounceTime(400)
+      .switchMap(event => this.configService.getFilm(event))
+      .subscribe(
+        data => {
+          console.log(data)
+          //  this.configService.getFilm(data).subscribe(response =>{
+              if((<any>data).Response == 'True'){
+                this.searchFilms = (<any>data).Search;
+                  this.filteredOptions = this.myControl.valueChanges
+                   .startWith('')
+                   .map(title => title.length >= 1 ? this.filter(title): title)
+                   .map(film => film && typeof film === 'object' ? film.Title : film)
+                   .map(title => title ? this.filter(title) : this.searchFilms.slice());
+                 }else{
+                   this.filteredOptions = Observable.empty<Response>();
+                  }
+      })
+  }
 
   ngOnInit() {}
-
-
 
   addToList(film:MatOptionSelectionChange,filmID:string){
     this.configService.listOfFilms(film,filmID)
   }
 
 
-  getListFilms(event :string){
-
-  if(event.length >= 2 || event != null){
-    this.myControl.valueChanges
-    .debounceTime(400)
-    //.switchMap()
-    .subscribe(data => {
-          this.configService.getFilm(data).subscribe(response =>{
-            if((<any>response).Response == 'True'){
-              this.searchFilms = (<any>response).Search;
-          //  console.log((<any>response).Response);
-              this.filteredOptions = this.myControl.valueChanges
-                 .startWith('')
-                 .map(Title => Title.length >= 1 ? this.filter(Title): Title)
-                 .map(film => film && typeof film === 'object' ? film.Title : film)
-                 .map(Title => Title ? this.filter(Title) : this.searchFilms.slice());
-               }
-           })
-      })
-    }else{
-      this.filteredOptions = Observable.empty<Response>();
-    }
-  }
-
-  filter(Title: string) {
-     return this.searchFilms.filter(option => Title);
+  filter(title: string) {
+    return this.searchFilms.filter(option => title);
   }
 
   displayFn(user): string {
-     return user ? user.Title : user;
+     return user ? user.title : user;
   }
 
 }
